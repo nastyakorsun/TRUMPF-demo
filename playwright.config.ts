8003dab7b7036884d741fb8836502aa812c55163
Anastasia Korsun <korsun@galaniprojects.de>
2024-01-31 13:53:02 +0100

    config

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,17 +10,11 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './e2e',
-  /* Run tests in files in parallel */
-  fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  testDir: '/Users/korsun/Desktop/TRUMPF-demo-neu/TRUMPF-demo/tests',
+ 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+  testMatch: '**/*.spec.ts',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
