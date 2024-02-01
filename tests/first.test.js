2a50004083e25afae245f67d9c2d53b70eb1188d
Anastasia Korsun <korsun@galaniprojects.de>
2024-02-01 11:42:29 +0100

    Remove node_modules and update .gitignore

--- tests/first.test.js
+++
@@ -1,9 +0,0 @@
-const { chromium } = require('playwright');
-
-(async () => {
-  const browser = await chromium.launch();
-  const page = await browser.newPage();
-  await page.goto('https://www.trumpf.com/de_DE/');
-  await page.screenshot({ path: `screenshot.png` });
-  await browser.close();
-})();
