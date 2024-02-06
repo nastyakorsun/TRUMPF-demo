105449f56af4c4f65f91823033bb3397b4e57891
Anastasia Korsun <korsun@galaniprojects.de>
2024-02-06 14:27:48 +0100

    test produktanfrage

---
+++ tests/Produktanfrage.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from '@playwright/test';
+
+test('Formulare Produktanfrage', async ({ page }) => {
+  await page.goto('https://www.trumpf.com/de_DE/');
+  await page.getByRole('button', { name: 'Alle Cookies akzeptieren' }).click();
+  await page.locator('li').filter({ hasText: 'Kontakt' }).getByRole('link').click();
+  await page.getByRole('link', { name: 'Jetzt kontaktieren' }).click();
+  await expect (page.getByRole('heading', { name: 'Sie interessieren sich für ein Produkt?' })).toBeVisible();
+  await page.locator('#powermail_field_produktanfrage-595634-salutation').selectOption('1');
+  await page.getByLabel('Vorname\n\t\t\t\t\n*').click();
+  await page.getByLabel('Vorname\n\t\t\t\t\n*').fill('Kate');
+  await page.getByLabel('Nachname\n\t\t\t\t\n*').click();
+  await page.getByLabel('Nachname\n\t\t\t\t\n*').fill('Testner');
+  await page.getByLabel('Firma\n\t\t\t\t\n*').click();
+  await page.getByLabel('Firma\n\t\t\t\t\n*').fill('Test');
+  await page.getByLabel('Straße und Nr.').click();
+  await page.getByLabel('Straße und Nr.').fill('Test');
+  await page.getByLabel('PLZ').click();
+  await page.getByLabel('PLZ').fill('12345');
+  await page.getByLabel('Ort').click();
+  await page.getByLabel('Ort').fill('Test');
+  await page.getByLabel('Land/Region\n                \n*').click();
+  await page.getByLabel('Land/Region\n                \n*').fill('d');
+  await page.getByText('Deutschland', { exact: true }).click();
+  await page.getByLabel('Telefon\n\t\t\t\t\n*').click();
+  await page.getByLabel('Telefon\n\t\t\t\t\n*').fill('1234567890');
+  await page.getByLabel('E-Mail\n\t\t\t\t\n*').click();
+  await page.getByLabel('E-Mail\n\t\t\t\t\n*').fill('test@testing.com');
+  await page.getByLabel('Folgende Produkte interessieren mich / Ihre Nachricht an uns:\n\t\t\t\t\n*').click();
+  await page.getByLabel('Folgende Produkte interessieren mich / Ihre Nachricht an uns:\n\t\t\t\t\n*').fill('test');
+  await page.locator('#powermail_field_newsletter_0').check();
+  await page.getByRole('button', { name: 'Absenden' }).click();
+  await expect (page.getByText('Vielen Dank für Ihre Anfrage, wir werden uns mit Ihnen in Verbindung setzen.')).toBeVisible(); 
+});
